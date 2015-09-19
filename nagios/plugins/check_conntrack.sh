14266b0cb0db07a1c74b58ed4a0a33af4abb3c0e
chandlercord <chandler@Chandler-Cords-MacBook-Air.local>
2015-09-19 05:03:55 -0700

    Added usage message and optarg stuffs

--- nagios/plugins/check_conntrack.sh
+++ nagios/plugins/check_conntrack.sh
@@ -5,14 +5,24 @@
 #command:
 #$USER1$/check_nrpe -H $HOSTADDRESS$ -t 15 -c check_conntrack
 
-if [ $# != 2 ]; then
-        echo "Syntax: /usr/lib64/nagios/plugins/check_conntrack <WARN %> <CRIT %>"
-        echo 
-        echo "Example: /usr/lib64/nagios/plugins/check_conntrack 75 90"
-        exit 1
+USAGE="Usage:\t \$ bash $0 -w <WARN %> -c <CRIT %>\nExample: \$ bash $0 -w 75 -c 90"
+
+if [ $# == 0 ]; then
+  echo -e $USAGE
+  exit 1
 fi
 
-if [ `find /proc/sys -name *conntrack_count | wc -l` -eq "0" ]; then echo "CRIT: Firewall is not running!"; exit 2; fi
+while getopts "c:w:h" ARG; do
+  case $ARG in
+    w) WARN=$OPTARG;;
+    c) CRIT=$OPTARG;;
+    h) echo -e $USAGE; exit 1;;
+  esac
+done
+
+if [ `find /proc/sys -name *conntrack_count | wc -l` -eq "0" ]; then
+  echo "CRIT: Firewall is not running!"; exit 2; 
+fi
 
 COUNT_FILE=`find /proc/sys -name *conntrack_count | head -n 1`
 MAX_FILE=`find /proc/sys -name *conntrack_max | head -n 1`
